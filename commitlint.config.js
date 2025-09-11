e703e1b4a7deff97235e1b2913ee13725ad1a8c1
qiyue <455450326@qq.com>
2025-09-11 17:51:26 +0800

    tool(其它变更): commitlint配置修改
    commitlint配置文件typeENUM增加类型

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,5 +9,6 @@ module.exports = {
     rules: {
         'type-empty': [2, 'never'],
         'subject-empty': [2, 'never'],
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'perf', 'test', 'tool', 'style', 'revert', 'update']],  // 添加 'tool'
     },
 };
