e2b01ea15b400d1b9835bafe173b52e37908a601
qiyue <455450326@qq.com>
2025-09-12 10:57:51 +0800

    tool(其它变更): sten库增加sass编译

--- sten-components/stencil.config.ts
+++ sten-components/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass'
 
 export const config: Config = {
   namespace: 'sten-components',
@@ -23,4 +24,9 @@ export const config: Config = {
   testing: {
     browserHeadless: "shell",
   },
+  plugins: [
+    sass({
+      // injectGlobalPaths 数组 全局通用的sass变量
+    })
+  ]
 };
